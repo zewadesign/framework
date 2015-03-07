b3819208a8b1c0831194dd664f94f2e987a6f510
jhoughtelin <josh@findsomehelp.com>
2015-03-07 15:30:39 -0700

    Removed unecessary DS global.

--- zechframework.php
+++ zechframework.php
@@ -1,12 +1,13 @@
 <?php
+// Composer Autoloader
 require __DIR__ . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
-// Define constants for file paths, url, etc.
-define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_PATH', __DIR__);
-define('APP_PATH', ROOT_PATH.DS.'app');
+define('APP_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'app');
 
-if (!ob_start("ob_gzhandler")) ob_start();
+if (!ob_start("ob_gzhandler")) {
+    ob_start();
+}
 
 $out = new \core\App();
 
