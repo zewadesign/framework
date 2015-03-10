fd40a9ff2c3264db4044d9970682f628c52351d4
Zechariah Walden <zech@zewadesign.com>
2015-03-10 17:43:33 -0500

    progress on native session handling

--- core/Request.php
+++ core/Request.php
@@ -98,7 +98,8 @@ class Request
      * @TODO: move flashdata to sessionhandler, make available here with other request vars still
      */
 
-    private $flashdataIdentifier;
+
+    private $flashdataId = '_z_session_flashdata';
 
 
     /**
@@ -111,14 +112,12 @@ class Request
         self::$instance = $this;
         $this->configuration = App::getConfiguration();
 
-        if ($this->configuration->session) {
-            // assume no flashdata
-            $this->flashdata = [];
-            $this->flashdataIdentifier = '_session_flashdata_12971';
-            $this->prepareFlashdata();
-            $this->session = $this->_normalize($_SESSION);
+        if($this->configuration->session->flashdataId) {
+            $this->flashdataId = $this->configuration->session->flashdataId;
         }
 
+        $this->registerFlashdata();
+        $this->session = $this->_normalize($_SESSION);
         $this->get = $this->_normalize($_GET);
         $this->post = $this->_normalize($_POST);
         $this->cookie = $this->_normalize($_COOKIE);
@@ -132,14 +131,13 @@ class Request
      * Processes current requests flashdata, recycles old.
      * @access private
      */
-    private function prepareFlashdata()
+    private function registerFlashdata()
     {
 
 
-        if (isset($_SESSION[$this->flashdataIdentifier])) {
-            // store them
+        if ($this->session($this->flashdataIdentifier)) {
 
-            $this->flashdata = unserialize($_SESSION[$this->flashdataIdentifier]);
+            $this->flashdata = unserialize($this->session($this->flashdataIdentifier));
             // and destroy the temporary session variable
             unset($_SESSION[$this->flashdataIdentifier]);
 
