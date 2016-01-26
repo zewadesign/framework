11652104114d494e1000ddc96fe84e07fd26b958
Zechariah Walden <zech@zewadesign.com>
2016-01-26 10:22:11 -0600

    updated router

--- Zewa/Router.php
+++ Zewa/Router.php
@@ -89,7 +89,7 @@ class Router
         $this->configuration = App::getConfiguration();
 
         $this->defaultModule = $this->configuration->modules->defaultModule;
-        $defaultModule = strtolower($this->defaultModule);
+        $defaultModule = $this->defaultModule;
         $this->defaultController = $this->configuration->modules->$defaultModule->defaultController;
         $this->defaultMethod = $this->configuration->modules->$defaultModule->defaultMethod;
 
