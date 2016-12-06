791e6910e78ffc1190b5b623dcfad3de26e6cf23
jhoughtelin <josh@findsomehelp.com>
2016-12-06 09:37:11 -0700

    Establishing full test coverage for the Config class.

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -12,7 +12,19 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
         $config = new Config($configPath);
 
         $expectedModuleConfig = require $configPath . "/Modules.php";
-        $this->assertSame($expectedModuleConfig,$config->get('modules'));
+        $this->assertSame($expectedModuleConfig, $config->get('modules'));
+    }
+
+    /**
+     * APP_PATH is set in the test bootstrap.php
+     */
+    public function testLoadConfigFromGlobalData()
+    {
+        // New Config without passing config folder path generates path from APP_PATH global
+        $config = new Config();
+
+        $expectedModuleConfig = require APP_PATH . "/Config/Modules.php";
+        $this->assertSame($expectedModuleConfig, $config->get('modules'));
     }
 
     /**
@@ -22,6 +34,17 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
     {
         $configPath = __DIR__ . "/fixtures/app/Config";
         $config = new Config($configPath);
+
         return $config->get('no_config_file_by_this_name');
+    }
+
+    public function testSetGetConfigItem()
+    {
+        $config = new Config();
+
+        $configItem = ['oneSetting' => 'oneValue', 'twoSettings' => 'twoValues'];
+        $config->set('testConfigKey', $configItem);
+
+        $this->assertSame($configItem, $config->get('testConfigKey'));
     }
 }
