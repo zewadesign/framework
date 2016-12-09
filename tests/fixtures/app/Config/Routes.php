f495b92ccd5b8a99f31825c7ed4c5d6780cab9e8
Zech Walden <Zech Walden>
2016-12-09 03:58:01 -0600

    code styles

--- tests/fixtures/app/Config/Routes.php
+++ tests/fixtures/app/Config/Routes.php
@@ -6,8 +6,7 @@
 return [
     // Example Module Hello Method
     'hello/([A-Za-z0-9]+)' => 'example/home/hello/$1',
-    'say/hello/callback' => function() : string
-    {
+    'say/hello/callback' => function() : string {
         return 'hello';
     },
     'say/hello' => ['\\Zewa\\Tests\\FixtureClass', 'hello']
